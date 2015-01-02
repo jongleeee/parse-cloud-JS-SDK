0980729c589da9d6456444ac5da676357b691830
jongleee <lee.jongyun13@gmail.com>
2015-01-02 14:58:22 -0300

    added updating list

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,29 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("updateMember", function(request, response) {
+    var userID = request.params.userID;
+
+	var userQuery = new Parse.Query(Parse.User);
+	
+	userQuery.equalTo('objectID', userID);
+	userQuery.first().then(function(user) {
+    	if (user) {
+        	// do something with the user here
+        	user.set('groups', request.params.groupList);
+        	
+			
+    		Parse.Cloud.useMasterKey();
+			
+			user.save().then(function() {
+            response.success();
+        	});
+    	
+		} else {
+        	// no match found, handle it or do response.error();
+    	}
+	});
+});
+
+
